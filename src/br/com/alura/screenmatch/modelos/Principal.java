aeda59b0de4275d6d1373777889bd1a3202674cd
Xenar99 <srxmarcelox@outlook.com>
2025-02-16 18:02:09 -0300

    Commit aula 3

--- src/br/com/alura/screenmatch/modelos/Principal.java
+++ src/br/com/alura/screenmatch/modelos/Principal.java
@@ -3,19 +3,29 @@ package br.com.alura.screenmatch.modelos;
 public class Principal {
 
     public static void main(String[] args) {
-        Filme favorito = new Filme();
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O Poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
-        favorito.setNome("The Matrix");
-        favorito.setAnoDeLancamento(1999);
-        favorito.setDuracaoEmMinutos(135);
-        favorito.setIncluidoNoPlano(true);
 
-        favorito.exibeFichaTecnica();
-        favorito.avalia(9);
-        favorito.avalia(8);
-        favorito.avalia(9);
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
 
-        System.out.println("Média de avaliações do filme: " +favorito.pegaMedia());
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
     }
 
 }
